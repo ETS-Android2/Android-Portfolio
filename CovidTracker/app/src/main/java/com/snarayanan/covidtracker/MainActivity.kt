3b74e3ad371854c9e93d274d802b2ab3e636c2a9
Sriram Narayanan <sriram210@yahoo.in>
2021-09-18 18:03:02 -0400

    MainActivity in progress

--- CovidTracker/app/src/main/java/com/snarayanan/covidtracker/MainActivity.kt
+++ CovidTracker/app/src/main/java/com/snarayanan/covidtracker/MainActivity.kt
@@ -3,24 +3,34 @@ package com.snarayanan.covidtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.google.gson.GsonBuilder
+import com.snarayanan.covidtracker.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 private const val BASE_URL = "https://covidtracking.com/api/v1/"
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var adapter: CovidSparkAdapter
+    private lateinit var binding: ActivityMainBinding
     private lateinit var perStateDailyData: Map<String, List<CovidData>>
     private lateinit var nationalDailyData: List<CovidData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val gson = GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create()
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -39,8 +49,11 @@ class MainActivity : AppCompatActivity() {
                     Log.w(TAG, "Did not receive a valid response body")
                     return
                 }
+
+                setupEventListeners()
                 nationalDailyData = nationalData.reversed()
                 Log.i(TAG, "Update graph with national data")
+                updateDisplayWithData(nationalDailyData)
             }
 
             override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
@@ -69,5 +82,59 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun setupEventListeners() {
+        // Add a listener for user scrubbing on the chart
+        binding.sparkView.isScrubEnabled = true
+        binding.sparkView.setScrubListener {
+            itemData -> if (itemData is CovidData) {
+                updateInfoForDate(itemData)
+        }
+        }
+        // Respond to radio button selected events
+        binding.radioGroupTimeSelection.setOnCheckedChangeListener{ _, checkedId ->
+            adapter.daysAgo = when(checkedId) {
+                R.id.radioButtonWeek -> TimeScale.WEEK
+                R.id.radioButtonMonth -> TimeScale.MONTH
+                else -> TimeScale.MAX
+            }
+
+            adapter.notifyDataSetChanged()
+        }
+        binding.radioGroupMetricSelection.setOnCheckedChangeListener{ _,checkedId ->
+            when (checkedId) {
+                R.id.radioButtonPositive -> updateDisplayMetric(Metric.POSITIVE)
+                R.id.radioNegative -> updateDisplayMetric(Metric.NEGATIVE)
+                R.id.radioButtonMax -> updateDisplayMetric(Metric.DEATH)
+            }
+        }
+    }
+
+    private fun updateDisplayMetric(metric: Metric) {
+        adapter.metric
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun updateDisplayWithData(dailyData: List<CovidData>) {
+        // Create a spark adapter
+        adapter = CovidSparkAdapter(dailyData)
+        // Update radio buttons to select the positive vases and max time by default
+        binding.sparkView.adapter = adapter
+        binding.radioNegative.isChecked = true
+        binding.radioButtonMax.isChecked = true
+        // Display metric for the most recent date
+        updateInfoForDate(dailyData.last())
+    }
+
+    private fun updateInfoForDate(covidData: CovidData) {
+        val numCases = when (adapter.metric){
+            Metric.NEGATIVE -> covidData.negativeIncrease
+            Metric.POSITIVE -> covidData.positiveIncrease
+            Metric.DEATH -> covidData.deathIncrease
+        }
+        binding.tvMetricLabel.text = NumberFormat.getInstance().format(covidData.positiveIncrease)
+        val outputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
+        binding.tvDateLabel.text = outputDateFormat.format(covidData.dateChecked)
     }
 }
