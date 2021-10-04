d9640d854a58eb3464861576f989abd77ddb0b94
Sriram Narayanan <sriram210@yahoo.in>
2021-10-03 21:40:49 -0400

    started SimpleTodo

---
+++ SimpleTodo/app/src/main/java/com/snarayanan/simpletodo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.snarayanan.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        items = new ArrayList<>();
+        items.add("Buy Milk");
+        items.add("Read Book");
+        items.add("Watch TV");
+
+
+
+    }
+}
